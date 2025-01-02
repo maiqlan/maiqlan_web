f147d48fb5b501a78baf8987623634d80db19d21
lan <mai.lauren2@gmail.com>
2025-01-02 16:50:43 -0600

    Navbar close and reopen when clicking off

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -3,12 +3,20 @@ import React, {useState} from "react";
 import styles from "./Navbar.module.css"
 import { getImageUrl } from "../../utils";
 export const Navbar = () => {
+    /**
+     *  we have state management for if we are in mobile
+     *  view, so when the img is clicked, we display navigation.
+     * create state value and state function
+     */
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <nav className={styles.navbar}>
             <a className={styles.title} href="/">Portfolio</a>
             <div className={styles.menu}>
-                <img className={styles.menuBtn} src={getImageUrl("nav/menuIcon.png")} alt="menu-button"/>
-                <ul className={styles.menuItems}>
+                <img className={styles.menuBtn} src={menuOpen ? getImageUrl("nav/closeIcon.png") : getImageUrl("nav/menuIcon.png") } alt="menu-button" onClick={() => setMenuOpen(!menuOpen)}/>
+                <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`} onClick={() => setMenuOpen(!menuOpen)}>
                     <li> 
                         <a href="#about">About</a>
                     </li>
