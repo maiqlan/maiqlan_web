868b21449c9139ff0434f7b9b428256b15e9a37f
lan <mai.lauren2@gmail.com>
2025-01-03 10:34:02 -0600

    bio

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styles from './App.module.css'
+import { Hero } from './component/Hero/Hero'
 import { Navbar } from './component/Navbar/Navbar'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
   <div className={styles.App}>
     <Navbar />
+    <Hero/>
   </div>    
   )
 }
---
+++ src/component/Hero/Hero.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './Hero.module.css'
+import { getImageUrl } from '../../utils';
+
+export const Hero = () => {
+    return (
+        <section className={styles.container}>
+            <div className={styles.content}>
+                <h1 className={styles.title}>Hello! My name is Ren Mai.</h1>
+                <p className={styles.description}>
+                    I'm currently a Fourth-Year computer science student at Texas A&M with a minor in mathematics. <br /><br />With regards to my areas of interest, I enjoy projects with a focus in machine learning, cryptography, parallel computing, reverse engineering, and distributed system concepts. 
+                </p>
+                <a href="Mailto:mai.lauren2@gmail.com" className={styles.contactBtn}>Contact Me</a>
+            </div>
+            <img className={styles.heroImg} src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" />
+            <div className={styles.topBlur}/>
+            <div className={styles.bottomBlur}/>
+        </section>
+    )
+}
